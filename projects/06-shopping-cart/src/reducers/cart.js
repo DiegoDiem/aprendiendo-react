5f343dc45b2f6e3eb1d3d1f02c256ad8c6143df8
DiegoDiem <ldmg.9@hotmail.com>
2023-03-16 22:58:23 -0600

    Diferentes formas de sumar codigo +  fix clear car

--- projects/06-shopping-cart/src/reducers/cart.js
+++ projects/06-shopping-cart/src/reducers/cart.js
@@ -20,9 +20,29 @@ export const cartReducer = (state, action) => {
       const productInCartIndex = state.findIndex(item => item.id === id)
 
       if (productInCartIndex >= 0) {
-        // Una forma sería usando structuredClone
-        const newState = structuredClone(state)
-        newState[productInCartIndex].quantity += 1
+        // ? Una forma sería usando structuredClone
+        // const newState = structuredClone(state)
+        // newState[productInCartIndex].quantity += 1
+        // updateLocalStorage(newState)
+
+        // ? Usando el map
+        // const newState = state.map(item => {
+        //   if (item.id === id) {
+        //     return {
+        //       ...item,
+        //       quantity: item.quantity + 1
+        //     }
+        //   }
+        //   return item
+        // })
+
+        // ? usando el spread operator y slice
+        const newState = [
+          ...state.slice(0, productInCartIndex),
+          { ...state[productInCartIndex], quantity: state[productInCartIndex].quantity + 1 },
+          ...state.slice(productInCartIndex + 1)
+        ]
+
         updateLocalStorage(newState)
         return newState
       }
@@ -45,8 +65,8 @@ export const cartReducer = (state, action) => {
     }
 
     case CART_ACTION_TYPES.CLEAR_CART: {
-      updateLocalStorage(cartInitialState)
-      return cartInitialState
+      updateLocalStorage([])
+      return []
     }
   }
 
